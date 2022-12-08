07c619c5d9b394852bf86f272dee0bc478790a47
Xoje <dawxoje@gmail.com>
2022-12-08 15:29:11 +0100

    first commit

---
+++ libs/common-ui/src/lib/form/form.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nx-tienda-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.scss'],
+})
+export class FormComponent {}
